125d23738daed1305a07e715cc6b136be0bd9605
Yura Chekhov <ychekhov@bk.ru>
2021-11-30 02:53:57 +0300

    style select

--- src/app/map.js
+++ src/app/map.js
@@ -10,3 +10,30 @@ for (let i = 0; i < found.length; i++) {
         }
     });
 }
+
+
+// let select = function () {
+//     let selectHeader = document.querySelectorAll('.search-select__header');
+//     let selectItem = document.querySelectorAll('.search-select__item');
+
+//     selectHeader.forEach(item => {
+//         item.addEventListener('click', function () {
+//             this.parentElement.classList.toggle('is-active');
+//         })
+//     })
+
+//     selectItem.forEach(item => {
+//         item.addEventListener('click', function () {
+//             let text = this.innerText, currentText = this.closest('.search-select').querySelector('.search-selector__current');
+//             text = currentText.innerText;
+//         })
+//     })
+// }
+
+select();
+// let sel = document.getElementsByClassName('search-select__header');
+// for (let i = 0; i < sel.length; i++) {
+//     sel.addEventListener('click', function () {
+//         this.parentElement.classList.toggle('is-active');
+//     })
+// }
