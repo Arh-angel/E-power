937d04c960f365039b8f89275e5db2b3ac5a27c9
Kholev Alexandr <khalex8993@gmail.com>
2021-12-07 23:08:07 +0300

    add adaptive home page class lock

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function toggelClass() {
     let nameClass = document.querySelectorAll(".header__burger, .mainmenu__list");
+    let pageScroll = document.querySelectorAll(".page");
 
-    console.log(nameClass)
+    console.log(nameClass);
+    console.log(pageScroll);
+
+    pageScroll.forEach(element => {element.classList.toggle('lock')})
 
     nameClass.forEach(element => {
         return element.classList.toggle('active');
